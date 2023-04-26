7e7a1bc4ef4cf0129f87fefd91ad88bae3e6f069
Claire Siddiqui <csi@zmtnb0121.local>
2023-04-26 14:17:43 +0200

    DIG-586: added hello world and  training data to api

--- src/api.py
+++ src/api.py
@@ -8,7 +8,23 @@ app = Flask(__name__)
 
 CORS(app)
 
+
+training_data = pd.read_csv(os.path.join('data', 'auto-mpg.csv'))
+
+print(training_data.head())
+
 @app.route("/", methods=["GET"])
 def index():
     return {"hello": "world"}
+
+
+@app.route("/hello_world", methods=["GET"])
+def hello_world():
+    return "<p>Hello world!<p>"
+
+
+@app.route("/training_data", methods=["GET"])
+def get_training_data():
+    return Response(training_data.to_json(), mimetype="application/json")
+
 
