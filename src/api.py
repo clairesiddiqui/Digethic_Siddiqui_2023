b85b6651fe17052ca2915620bc546166fa47494d
Claire Siddiqui <csi@zmtnb0121.local>
2023-04-26 14:47:44 +0200

    DIG-587: changed api.py file

--- src/api.py
+++ src/api.py
@@ -10,8 +10,15 @@ CORS(app)
 
 
 training_data = pd.read_csv(os.path.join('data', 'auto-mpg.csv'))
-
 print(training_data.head())
+
+file_to_open = open(os.path.join("data", "models", "baummethoden_lr.pickle"), 'rb')
+trained_model = pickle.load(file_to_open)
+file_to_open.close()
+
+
+
+
 
 @app.route("/", methods=["GET"])
 def index():
@@ -28,3 +35,17 @@ def get_training_data():
     return Response(training_data.to_json(), mimetype="application/json")
 
 
+@app.route("/predict", methods=["GET"])
+def predict():
+    zylinder = request.args.get("zylinder")
+    ps = request.args.get('ps')
+    gewicht = request.args.get('gewicht')
+    beschleunigung = request.args.get('beschleunigung')
+    baujahr = request.args.get('baujahr')
+
+    if(zylinder and ps and gewicht and beschleunigung and baujahr):
+        prediction = trained_model.predict([[zylinder, ps, gewicht, beschleunigung, baujahr]])
+        print(prediction)
+        return {"result": prediction[0]}
+
+    return Response("Please provide all necessary parameters to get prediction result!")
